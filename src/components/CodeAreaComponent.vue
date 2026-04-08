a395ae542d5adc56c0bb81b6eed4f641c8538ff3
Sviat6239 <sviatoslav6239@gmail.com>
2026-04-08 20:15:11 +0200

    added line counter

--- src/components/CodeAreaComponent.vue
+++ src/components/CodeAreaComponent.vue
@@ -1,11 +1,30 @@
 <script setup>
-  import "../styles/CodeArea.css"
-</script>
+import { computed, ref } from "vue";
+import "../styles/CodeArea.css";
 
+const text = ref("");
+const editorRef = ref(null);
+const linesRef = ref(null);
+
+const lineCount = computed(() => text.value.split("\n").length);
+
+function syncScroll() {
+  if (!editorRef.value || !linesRef.value) {
+    return;
+  }
+
+  linesRef.value.scrollTop = editorRef.value.scrollTop;
+}
+</script>
 <template>
   <div class="code-area">
-    <textarea name="" id="" cols="50" rows="50" class="editor"></textarea>
+    <div class="line-count" ref="linesRef">
+      <div class="line-number" v-for="n in lineCount" :key="n">{{ n }}</div>
+    </div>
+    <textarea ref="editorRef" class="editor" v-model="text" @scroll="syncScroll"></textarea>
+
   </div>
+
 </template>
 
 <style scoped>
