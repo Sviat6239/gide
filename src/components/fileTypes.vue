853bf5e22eddb36b8f962cb0d98160c2620bbaf3
Sviat6239 <sviatoslav6239@gmail.com>
2026-05-17 14:20:27 +0200

    refactored file templates

---
+++ src/components/fileTypes.vue
@@ -0,0 +1,58 @@
+<script>
+const fileTypes = {
+  txt: {
+    label: 'Text File',
+    extensions: ['.txt'],
+  },
+  md: {
+    label: 'Markdown',
+    extensions: ['.md'],
+  },
+  rust: {
+    label: 'Rust File',
+    extensions: ['.rs'],
+  },
+  python: {
+    label: 'Python File',
+    extensions: ['.py'],
+  },
+  lua: {
+    label: 'Lua File',
+    extensions: ['.lua'],
+  },
+  gamma: {
+    label: 'Gamma File',
+    extensions: ['.gamma', '.gma', '.gm'],
+  },
+  html: {
+    label: 'HTML File',
+    extensions: ['.html', '.htm'],
+  },
+  css: {
+    label: 'CSS File',
+    extensions: ['.css'],
+  },
+  js: {
+    label: 'JavaScript File',
+    extensions: ['.js', '.jsx'],
+  },
+  ts: {
+    label: 'TypeScript File',
+    extensions: ['.ts', '.tsx'],
+  },
+  vue: {
+    label: 'Vue File',
+    extensions: ['.vue'],
+  },
+  ccpp: {
+    label: 'C/C++ File',
+    extensions: ['.c', '.cpp', '.h', '.hpp'],
+  },
+  asm: {
+    label: 'Assembly File',
+    extensions: ['.asm', '.assembly', '.as', '.s'],
+  },
+};
+
+export default fileTypes;
+</script>
