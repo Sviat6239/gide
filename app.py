eff30e412b52bf089a99d9d446826e042f206702
Sviat6239 <sviatoslav6239@gmail.com>
2026-07-21 17:33:34 +0200

    Refactor main application structure to implement QMainWindow for better GUI management and layout organization

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from PySide6.QtWidgets import QApplication, QWidget
 import sys
+from PySide6.QtCore import QSize
+from PySide6.QtWidgets import QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("G-IDE")
+        self.setFixedSize(QSize(400, 300))
+
+        self.setStyleSheet(
+            "color: white;"
+        )
+
+        layout = QVBoxLayout()
+        self.codearea = QTextEdit()
+        layout.addWidget(self.codearea)
+        
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
 
 app = QApplication(sys.argv)
-
-window = QWidget()
+window = MainWindow()
 window.show()
-
 app.exec()
